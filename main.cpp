7aa555cd8db30d7ec39ffb09a5ce5a04c4ac8845
Narek2481 <narekengibaryan.1@mail.ru>
2022-10-07 22:43:19 +0400

    mek ev erkuhatanoc navery random texadrely

--- main.cpp
+++ main.cpp
@@ -2,104 +2,176 @@
 #include <cmath>
 
 
-using namespace std; 
-// dashty nkarelu funkcia 
-void map_screen(bool arr_vessel[][10],const int size){ 
+using namespace std;
+
+void map_screen(int arr_vessel[][10],const int size){ 
     for (int i = 0; i < size; i++)
     {
         for (int j = 0; j < size; j++)
         {
-            if(arr_vessel[i][j]){
-                cout << "+  ";
-            }else{
+            if(arr_vessel[i][j]==0){
                 cout << "-  ";
+            }else{
+                cout << arr_vessel[i][j] << "  ";
             }
         }
         cout << endl;
     }
     
 }
-//random 1 hatikanoc navery dnelu funkcia
-void vessel_1(bool arr_vessel[][10], int  arr_random_x [] ,int arr_random_y [],const int size) { 
+
+void circle_close(int arr_vessel[][10],int x,int y){
+   
+        if(y - 1 < 10 && y - 1 > -1){
+            if(arr_vessel[x][y - 1] == 0){
+                arr_vessel[x][y - 1] = 5;
+            }
+            
+        }
+        if(y + 1 < 10 && y + 1 > -1){
+            if(arr_vessel[x][y + 1] == 0){
+                arr_vessel[x][y + 1] = 5;
+            }
+            
+        }
+        if(x - 1 < 10 && x - 1 > -1){
+            if(arr_vessel[x - 1][y] == 0){
+                arr_vessel[x - 1][y] = 5;
+            }
+            
+        }
+        if(x + 1 < 10 && x + 1 > -1){
+            if( arr_vessel[x + 1][y] == 0){
+                arr_vessel[x + 1][y] = 5;
+            }
+            
+        }
+        if(y - 1 < 10 && y - 1 > -1 && x - 1 < 10 && x - 1 > -1){
+            if( arr_vessel[x - 1][y - 1] == 0){
+                arr_vessel[x - 1][y - 1] = 5;
+            }
+            
+        }
+        if(y + 1 < 10 && y + 1 > -1 && x + 1 < 10 && x + 1 > -1){
+            if(arr_vessel[x + 1][y + 1] == 0){
+                arr_vessel[x + 1][y + 1] = 5;
+            }
+            
+        }
+        if(y + 1 < 10 && y + 1 > -1 && x - 1 < 10 && x - 1 > -1){
+            if(arr_vessel[x - 1][y + 1] == 0){
+                arr_vessel[x - 1][y + 1] = 5;
+            }
+            
+
+        }
+        if(y - 1 < 10 && y - 1 > -1 && x + 1 < 10 && x + 1 > -1){
+            if(arr_vessel[x + 1][y - 1] == 0){
+                arr_vessel[x + 1][y - 1] = 5;
+            }
+            
+        }
     
-	int  point_ship = 1;
-	for (int i = 0; i < 4; i++)
-	{
-		arr_vessel[arr_random_x[i]][arr_random_y[i]]= true;
-	}
-	map_screen(arr_vessel,size);
 }
 
 
-
-
-
-
-void random_vessel (bool arr_vessel[][10], int size) { // random navery dasavorelu funkcia 
-	int arr_random_x [size];
-	int arr_random_y [size];
-
-    bool alredyThere_x;
-	bool alredyThere_y;
-    for (int i = 0; i < size;)
-    {
-        alredyThere_x = true;
-        int newRandomValue = rand() % 10;
-        
-        for (int j = 0; j < i ; j++)
-        {
-            if(arr_random_x[j] == newRandomValue){
-                alredyThere_x = false;
-            }
-        }
-        if(alredyThere_x){
-            arr_random_x[i] = newRandomValue;
-            i++;
-            
-        }  
-    }
-	
-	for (int i = 0; i < size;)
-    {
-        alredyThere_y = true;
-        int newRandomValue = rand() % 10;
-        
-        for (int j = 0; j < i ; j++)
-        {
-            if(arr_random_y[j] == newRandomValue){
-                alredyThere_y = false;
-            }
-        }
-        if(alredyThere_y){
-            arr_random_y[i] = newRandomValue;
-            i++;
-           
-        }  
-    }
-	vessel_1(arr_vessel,arr_random_x, arr_random_y,size);
-	
-
-
+// patahakan mek hatanoc navery texadrox funkcia 
+void ship_random_1(int arr_vessel[][10],const int size){ 
     
+    int x = rand()%10;
+    int y = rand()%10; 
+    if(arr_vessel[x][y] == 0){
+        arr_vessel[x][y] = 1;
+        circle_close(arr_vessel,x,y);
+        
+    }else{
+        ship_random_1(arr_vessel,size);
+    }
+
+}
+
+void chack_ship_random_2(int arr_vessel[][10],int x,int y){
+    int chack_arr[4][2];
+    
+    if(arr_vessel[x][y-1] == 0 && y-1 > 0){
+        chack_arr[0][0] = x;
+        chack_arr[0][1] = y-1;
+    }
+    
+    
+    if(arr_vessel[x][y+1] == 0 && y+1 < 10){
+        chack_arr[0][0] = x;
+        chack_arr[0][1] = y+1;
+    }
+    
+    
+    if(arr_vessel[x-1][y] == 0 && x-1 > 0){
+        chack_arr[0][0] = x-1;
+        chack_arr[0][1] = y;
+    }
+    
+    
+   
+    if(arr_vessel[x+1][y] == 0 && x+1 < 10){
+        chack_arr[0][0] = x+1;
+        chack_arr[0][1] = y;
+    }
+    
+    
+        
+    int k = rand() % 4;
+    if(chack_arr[k][0] < 10 && chack_arr[k][0] > -1 && chack_arr[k][1] < 10 && chack_arr[k][1] > -1){
+        arr_vessel[chack_arr[k][0]][chack_arr[k][1]] = 2;
+        circle_close(arr_vessel,chack_arr[k][0],chack_arr[k][1]);
+        circle_close(arr_vessel,x,y);
+    }else{
+        chack_ship_random_2(arr_vessel,x,y);
+    }
+}
+// erkuhatanoc navery texadrox funkcia 
+void ship_random_2(int arr_vessel[][10],const int size){ 
+    int ship_lenght = 2;
+    int x = rand()%10;
+    int y = rand()%10; 
+    
+    
+    if(arr_vessel[x][y] == 0){
+        
+        arr_vessel[x][y] = 2;
+        chack_ship_random_2(arr_vessel,x,y);
+    }else{
+        ship_random_2(arr_vessel,size);
+    }
+
 }
 
 
 
 
 int  main() {
+    srand(time(NULL));
 	const  int size = 10; // dashti erkararutyuny ev laynutyunt ev naveri qanaky 
-	bool arr_vessel[size] [size]; // qartezi vra naveri texakayuman zangvachy 
+	int arr_vessel[size] [size]; // qartezi vra naveri texakayuman zangvachy 
 	for (int i = 0; i < size; i++)
     {
         for (int j = 0; j < size; j++)
         {
-            arr_vessel[i][j] = false;
+            arr_vessel[i][j] = 0;
         }
         
     }
+    for (int i = 0; i < 4; i++)
+    {
+        ship_random_1(arr_vessel,size);
+    }
+    
+    ship_random_2(arr_vessel,size);
+    ship_random_2(arr_vessel,size);
+    ship_random_2(arr_vessel,size);
     
     
-	random_vessel(arr_vessel, size);
+
+	map_screen(arr_vessel,size);
 
 	
 
